fefec06221ac6d25b5405f8c974e7948e0d4bdf8
Gary Forster <gary@garyforster.io>
2020-05-15 10:25:09 +0900

    feat: add styled components and config fluid typography

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 
 import App from '/components/app';
 
@@ -7,5 +8,21 @@ import App from '/components/app';
 import '/services/i18n';
 // Normalize css
 import 'normalize.css';
+/**
+ * Fluid Typogrphy
+ * Can be reconfigured to not use min, max if targeting older browsers
+ * https://css-tricks.com/simplified-fluid-typography/
+ */
+const GlobalStyle = createGlobalStyle`
+  html {
+    font-size: min(max(16px, 4vw), 22px);
+  }
+`;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyle />
+    <App />
+  </>,
+  document.getElementById('root'),
+);
