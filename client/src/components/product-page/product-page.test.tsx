c4f2f755ea5c58a726cb6c0003f51bde6b2b9189
Gary Forster <gary@garyforster.io>
2020-05-16 17:49:00 +0900

    feat: add product page tests

---
+++ client/src/components/product-page/product-page.test.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import React from 'react';
+
+import '@testing-library/jest-dom/extend-expect';
+
+import * as hooks from '/hooks';
+import { render, screen } from '/utils/test.utils';
+
+import ProductPage from './product-page';
+
+const stateSpy = jest.spyOn(hooks, 'useApi');
+
+const DATA = {
+  id: '1',
+  name: 'Light pink shoes',
+  description:
+    'Pre-owned, in good condition with signs of wear, may just need to be cleaned or washed. Please see photos for style & condition.',
+  like_count: 91,
+  comment_count: 59,
+  price: 51,
+  is_sold_out: false,
+  shipping_fee: '送料込み',
+  image: 'http://localhost:5000/images/image_1.png',
+  category_id: 2,
+};
+
+describe('List', () => {
+  afterEach(() => {
+    stateSpy.mockReset();
+  });
+  it('it renders a loading screen', async () => {
+    stateSpy.mockReturnValue({
+      loading: true,
+      response: undefined,
+      error: undefined,
+    });
+    render(<ProductPage />);
+    expect(screen.getByText('Loading...')).toBeVisible();
+  });
+  it('displays item', async () => {
+    stateSpy.mockReturnValue({
+      loading: false,
+      response: DATA,
+      error: undefined,
+    });
+    render(<ProductPage />);
+    await screen.findByText(DATA.description);
+    expect(stateSpy).toHaveBeenCalledTimes(1);
+    expect(screen.getByAltText('thumbnail')).toHaveAttribute('src', DATA.image);
+  });
+  it('displays add to cart ribbon', async () => {
+    stateSpy.mockReturnValue({
+      loading: false,
+      response: DATA,
+      error: undefined,
+    });
+    render(<ProductPage />);
+    await screen.findByRole('button');
+    expect(stateSpy).toHaveBeenCalledTimes(1);
+    expect(screen.getByRole('button')).toHaveTextContent('ADD TO CART');
+  });
+});
