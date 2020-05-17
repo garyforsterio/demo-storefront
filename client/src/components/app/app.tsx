e44ee637abb217fbb4b00ec934e396a3f43f4670
Gary Forster <gary@garyforster.io>
2020-05-17 14:37:38 +0900

    feat: add category routes

--- client/src/components/app/app.tsx
+++ client/src/components/app/app.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
-import Items from '/routes/items';
+import Items from '/routes';
+import Categories from '/routes/categories/id';
 import Item from '/routes/items/id';
 
 const App: React.FunctionComponent = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route component={Categories} path="/categories/:id" />
         <Route component={Item} path="/items/:id" />
-        <Route component={Items} path="/items" />
+        <Route component={Items} path="/" />
         <Route path="*">
-          <Redirect to="/items" />
+          <Redirect to="/" />
         </Route>
       </Switch>
     </BrowserRouter>
